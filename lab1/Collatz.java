4b2f423eab19ea11d294a3937680dbe569060781
LanConste <daolanisss@gmail.com>
2025-10-28 19:06:58 +0800

    Update Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,13 +5,15 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (isPrime(n)) {
+            return n / 2; 
         } else {
-            return n * 2;
+            return 3 * n + 1; 
         }
+
+    }
+    public static boolean isPrime(int n) {
+        return (n % 2 == 0); 
     }
 
     public static void main(String[] args) {
